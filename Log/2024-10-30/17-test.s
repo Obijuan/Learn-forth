d0df6a7feb418df2f2257a63967e2df8af26f812
obijuan <obijuan.cube@gmail.com>
2024-10-30 18:50:06 +0100

    Update log

---
+++ Log/2024-10-30/17-test.s
@@ -0,0 +1,105 @@
+
+#-- Cambio/mejora: 
+#-- El codigo es el mismo que en el ejemplo anterior, pero se definen más
+#-- palabras a partir de otras, y se comprueba que se ejecutan OK
+#-- La palabra de más profundidad, TEST3, llama a EX para terminar
+#--
+#-- La caadena que se imprime en la consola es: ABABAB y se termina
+#-- Todo funciona perfecto
+
+    .include "so.s"
+
+#---------------------------------------------------
+#-- Definimos las palabras de nuestro sistema
+#---------------------------------------------------
+    .data
+
+TEST3:
+    .word DOCOL
+    .word W1
+    .word W2
+    .word EX
+
+TEST2:
+    .word DOCOL
+    .word W1
+    .word W2
+    .word TEST3
+
+    #-- Primera Palabra de prueba a Ejecutar
+    #-- Como es la primera es "especial". Su configuracion
+    #-- es diferente a la de las demas
+    #-- No tiene codeword propio
+TEST:   
+    .word W1   
+    .word W2     
+    .word TEST2
+
+#------------------------
+#-- W1: Imprimir A
+#------------------------
+    .data
+W1: .word code_W1   #-- Codeword: Direccion codigo ejecutable
+    #-- Implementacion de W1
+    .text
+code_W1:
+   SO_PRINT_CHAR('A')
+   NEXT     #-- Ejecutar siguiente instruccion
+
+#-----------------------
+# W2: Imprimir B 
+#-----------------------
+    .data
+W2: .word code_W2   #-- Codeword
+    .text
+code_W2:
+   SO_PRINT_CHAR('B')
+   NEXT  
+
+#-----------------------
+# EX: Terminar
+#-----------------------
+    .data
+EX: .word code_EX   #-- Codeword 
+    .text
+code_EX:
+   SO_PRINT_CHAR('\n')
+   SO_EXIT
+   #-- Es una instruccion especial
+   #-- Se termina, por lo que NO se llama a NEXT
+
+
+#-----------------------
+#-- DOCOL
+#-- NO es una palabra de Forth. Es directamente codigo
+#-- maquina que dice como ejecutar una palabra no primitiva
+#-----------------------
+.text
+DOCOL:
+   #-- a0 apunta a DOCOL, cuando se empieza a ejecutar
+   #-- Si le sumamos 4, apunta a la siguiente palabra
+   #-- (Una palabra que empieza por DOCOL esta formada por varias
+   #-- palabras. Es decir, que tras DOCOL hay una palabra seguro)
+   #-- Hacemos que s1 apunte a esa siguiente palabra
+   addi s1, a0, 4  #-- S1 apunta a la siguiente palabra tras DOCOL
+
+   #-- Ejecutar la siguiente instruccion!
+   NEXT
+
+#-------------------------------------------
+#-- Programa principal
+#-- ARRANCA AQUI!!!!
+#-------------------------------------------
+    .text
+    .global main
+main:
+
+    #-- S1 apunta la primera palabra a ejecutar (W1)    
+    la s1,TEST  #-- s1 apunta a la Variable con la palabra a ejecutar
+                #-- s1 es el IP (Puntero de instruccion)
+
+    #-- Ejecutar primera instruccion (W1)
+    NEXT  #-- Ejecuta la instruccion apuntada por s1
+          #-- y Apuntar a la siguiente
+
+    #-- Nunca llega aqui!!!!!
