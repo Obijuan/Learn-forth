feb811de4541fe237e867404b526f532c71f0d42
obijuan <obijuan.cube@gmail.com>
2024-11-03 20:51:09 +0100

    add 39-test

---
+++ Log/2024-11-03/test.f
@@ -0,0 +1,15 @@
+\\-- Probando DOTS
+1 2 3 .S
+
+\\-- Probando DROP
+DROP DROP DROP .S
+
+\\-- Probando  SWAP
+5 6 .S SWAP .S DROP DROP .S
+
+\\-- Prueba de DUP
+5 .S DUP .S DROP DROP .S
+
+\\-- Prueba de OVER
+1 2 3 .S OVER .S DROP DROP DROP DROP .S 
+
