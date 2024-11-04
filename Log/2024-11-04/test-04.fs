899f4801f3cbb50ea34a07d50db1a6ea3f23c3a8
obijuan <obijuan.cube@gmail.com>
2024-11-04 22:13:22 +0100

    add 44-test

---
+++ Log/2024-11-04/test-04.fs
@@ -0,0 +1,11 @@
+\ Probar operando AND
+240 31 .S AND .S DROP .S
+
+\ Probar operando OR
+240 15 .S OR .S DROP .S
+
+\ Probar operando XOR
+170 255 .S XOR .S DROP .S
+
+\ Probar operando INVERT
+170 .S INVERT .S DROP .S
