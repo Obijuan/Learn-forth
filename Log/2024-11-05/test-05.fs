10f0a655ae55946cf59ddcb69b41d991ace62876
obijuan <obijuan.cube@gmail.com>
2024-11-05 18:05:42 +0100

    add 46-test

---
+++ Log/2024-11-05/test-05.fs
@@ -0,0 +1,21 @@
+\ Probar VERSION
+VERSION .S DROP .S
+
+\ Probar R0
+R0 .S DROP .S
+
+\ Probar DOCOL
+DOCOL .S DROP .S 
+
+\ Probar F_IMMED
+F_IMMED .S DROP .S
+
+\ Probar F_HIDDEN
+F_HIDDEN .S DROP .S
+
+\ Probar F_LENMASK
+F_LENMASK .S DROP .S
+
+\ Probar >R
+1973 .S >R .S
+
