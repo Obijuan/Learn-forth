0b0aa4a763bded2dbfdfe4c9a5bd5c9d81a9e2ca
obijuan <obijuan.cube@gmail.com>
2024-11-05 15:32:27 +0100

    update 45-test

---
+++ Log/2024-11-05/test-04.fs
@@ -0,0 +1,27 @@
+\ Probar operando AND
+240 31 .S AND .S DROP .S
+
+\ Probar operando OR
+240 15 .S OR .S DROP .S
+
+\ Probar operando XOR
+170 255 .S XOR .S DROP .S
+
+\ Probar operando INVERT
+170 .S INVERT .S DROP .S
+
+\ Probar !
+255 268697600 .S ! .S   \ mem[268697600] = 255 (0x10040000)  
+
+\ Probar @
+268697600 @ .S DROP .S  \ Leer mem[268697600]
+
+\ Probar +!
+5 268697600 +! 268697600 @ .S DROP .S 
+
+\ Probar -!
+10 268697600 -! 268697600 @ .S DROP .S 
+
+\ Probar C!
+55 268697600 .S C! .S  \Leer mem[268697600]  
+
