442c1e429461981fbf34334d8794b393d4e38d8c
obijuan <obijuan.cube@gmail.com>
2024-11-06 20:35:44 +0100

    add 47-test

---
+++ Log/2024-11-06/test-05.fs
@@ -0,0 +1,30 @@
+\ Probar VERSION
+VERSION .S DROP .S
+
+\ Probar R0
+R0 .S DROP .S
+
+\ Probar DOCOL
+DOCOL .S DROP .S 
+
+\ Probar F_IMMED
+F_IMMED .S DROP .S
+
+\ Probar F_HIDDEN
+F_HIDDEN .S DROP .S
+
+\ Probar F_LENMASK
+F_LENMASK .S DROP .S
+
+\ Probar >R
+1973 .S >R .S
+
+\ Probar R>
+R> .S DROP .S
+
+\ Probar RSP@
+RSP@ .S DROP .S
+
+\ Probar DSP@
+DSP@ .S DROP .S
+
