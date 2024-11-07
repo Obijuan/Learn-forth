d4fafedda2aba169d7c6d04153726606a4ddc22d
obijuan <obijuan.cube@gmail.com>
2024-11-07 20:58:30 +0100

    add 50-test

--- Log/2024-11-07/jonesforth.fs
+++ Log/2024-11-07/jonesforth.fs
@@ -10,3 +10,17 @@
 : '\n' 10 ;
 : BL   32 ; \ BL (BLank) es la palabra FORTH standard para el espacio
 
+\ CR Imprimir un salto de linea
+: CR '\n' EMIT ;
+
+\ SPACE Imprimir un espacio  
+: SPACE BL EMIT ;
+
+\ NEGATE Cambiar el signo de un entero
+: NEGATE 0 SWAP - ;
+
+\ Palabras estandar para BOOLEANOS
+: TRUE  1 ;
+: FALSE 0 ;
+: NOT   0= ;  \-- Operacion NOT con lo que hay en la pila
+
--- Log/2024-11-07/test-06.fs
+++ Log/2024-11-07/test-06.fs
@@ -7,4 +7,15 @@
 \ Probar el modulo MOD
 10 3 .S MOD .S DROP .S 
 
+\ Probar CR y SPACE
+CR CR CR SPACE SPACE SPACE .S
+
+\ Probar NEGATE
+73 .S NEGATE .S DROP .S  
+
+\ Probar TRUE y FALSE
+TRUE FALSE .S DROP DROP .S 
+
+
+
 
